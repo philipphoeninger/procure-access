da49fa565b8b0edd473b79538a13811e6d53efd0
philipphoeninger <44400490+philipphoeninger@users.noreply.github.com>
2025-10-14 00:41:54 +0000

    add some shared state features

---
+++ CLIENT/CLIENT.ProcureAccess/src/app/shared/state/with-loading.ts
@@ -0,0 +1,32 @@
+import { computed } from '@angular/core';
+import {
+  patchState,
+  signalStoreFeature,
+  withComputed,
+  withMethods,
+  withState
+} from '@ngrx/signals';
+
+export const withLoading = () {
+  return signalStoreFeature(
+    withState({ loadingCount: 0 }),
+    withMethods((state) => ({
+      incrementLoadingCount() {
+        patchState(state, {
+          loadingCount: state.loadingCount() + 1
+        });
+      },
+      decrementLoadingCount() {
+        patchState(state, {
+          loadingCount: state.loadingCount() - 1
+        });
+      }
+    })),
+    withComputed((state) => ({
+      isLoading: computed(() => {
+        return state.loadingCount() > 0;
+      })
+    }))
+  );
+}
+
