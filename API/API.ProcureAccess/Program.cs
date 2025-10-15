91de5090a085b66a483ab20fa35f01d045740dc8
philipphoeninger <44400490+philipphoeninger@users.noreply.github.com>
2025-10-15 18:28:01 +0000

    fix some naming issue

--- API/API.ProcureAccess/Program.cs
+++ API/API.ProcureAccess/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers()
                 .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
 builder.Services.AddSwaggerExplorer(builder.Configuration)
-                .AddFileSharingApiVersionConfiguration(new ApiVersion(0, 1))
+                .AddProcureAccessApiVersionConfiguration(new ApiVersion(0, 1))
                 .AddSqlServerConnection(builder.Configuration)
                 .AddAppConfig(builder.Configuration)
                 .AddCors()
