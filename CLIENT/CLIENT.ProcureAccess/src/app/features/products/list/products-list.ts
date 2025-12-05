bb27568c0212d65d206421795ade0d21581b6287
philipphoeninger <44400490+philipphoeninger@users.noreply.github.com>
2025-12-05 13:51:08 +0100

    create products list page

---
+++ CLIENT/CLIENT.ProcureAccess/src/app/features/products/list/products-list.ts
@@ -0,0 +1,93 @@
+import { LiveAnnouncer } from '@angular/cdk/a11y';
+import { Component, inject, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDividerModule } from '@angular/material/divider';
+import { ProcureAccessStore } from '@app/core/state/app.store';
+import { products } from '../data/dummy-data';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { SaveSearchDialog } from '../dialogs/save-search-dialog/save-search-dialog';
+
+@Component({
+  selector: 'pa-products-list',
+  imports: [
+    FormsModule,
+    MatDividerModule,
+    MatTableModule, 
+    MatButtonModule,
+    MatSortModule,
+    MatIconModule
+  ],
+  templateUrl: './products-list.html',
+  styleUrl: './products-list.scss'
+})
+export class ProductsList {
+  protected store = inject(ProcureAccessStore);
+  private _liveAnnouncer = inject(LiveAnnouncer);
+
+  displayedColumns: string[] = ['id', 'name', 'link', 'functionality', 'type'];
+  dataSource = new MatTableDataSource(products);
+
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
+  readonly dialog = inject(MatDialog);
+  private _snackBar = inject(MatSnackBar);
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
+
+    /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
+  }
+
+  linkCopied() {
+    this.showSnackbar('Link was copied to clipboard!');
+  }
+
+  exportProductsFile(){
+    const link = document.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', './');
+    link.setAttribute('download', `products.csv`);
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+}
+
+  openProductsSearchDialog(): void {
+    const dialogRef = this.dialog.open(SaveSearchDialog, {
+      width: '420px'
+    });
+
+    dialogRef.afterClosed().subscribe((name: string) => {
+      if (name !== undefined) {
+        this.showSnackbar('The products were saved under: ' + name);
+      }
+    });
+  }
+
+  // TODO: make it a service
+  showSnackbar(message: string) {
+    this._snackBar.open(message, 'Close', {
+      horizontalPosition: 'center',
+      verticalPosition: 'top'
+    });
+  }
+}
