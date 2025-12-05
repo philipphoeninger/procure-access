e8cf9a05170d810c54193fdb2630c6fd3494d56e
philipphoeninger <44400490+philipphoeninger@users.noreply.github.com>
2025-12-05 13:54:33 +0100

    add criterion model and criteria dummy data

---
+++ CLIENT/CLIENT.ProcureAccess/src/app/features/criteria/data/dummy-data.ts
@@ -0,0 +1,19 @@
+import { Criterion } from "../models/criterion.model";
+
+export const criteria: Criterion[] = [
+    {
+        id: 0,
+        name: "Test Criterion #1",
+        description: "Test Description #1"
+    },
+    {
+        id: 1,
+        name: "Test Criterion #2",
+        description: "Test Description #2"
+    },
+    {
+        id: 2,
+        name: "Test Criterion #3",
+        description: "Test Description #3"
+    },
+];
