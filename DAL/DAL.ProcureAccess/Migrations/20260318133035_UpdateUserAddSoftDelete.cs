2cf53fb3c74a7020c8dc3aba6efe060bb055a909
philipphoeninger <44400490+philipphoeninger@users.noreply.github.com>
2026-03-18 15:00:38 +0100

    add update user soft deletion migration

---
+++ DAL/DAL.ProcureAccess/Migrations/20260318133035_UpdateUserAddSoftDelete.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.ProcureAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserAddSoftDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                schema: "Identity",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                schema: "Identity",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                schema: "Identity",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                schema: "Identity",
+                table: "Users");
+        }
+    }
+}
