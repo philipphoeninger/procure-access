db480cbab68ae79744b016e76d13c5558b9c2a2a
philipphoeninger <44400490+philipphoeninger@users.noreply.github.com>
2026-04-15 20:42:02 +0200

    remove approval func from object controllers

--- API/API.ProcureAccess/Controllers/CriteriaController.cs
+++ API/API.ProcureAccess/Controllers/CriteriaController.cs
@@ -34,44 +34,4 @@ public class CriteriaController : BaseCrudController<Criterion, CriterionDto, Cr
         var filteredCriteria = _criterionService.GetByCriteriaFilterIds(criteriaFilterIds);
         return Ok(filteredCriteria);
     }
-
-    /// <summary>
-    /// Updates & approves a single record
-    /// </summary>
-    /// <remarks>
-    /// <pre>
-    /// </pre>
-    /// </remarks>
-    /// <param name="dto">Dto of Entity to update & approve</param>
-    /// <returns>Single record</returns>
-    [HttpPut("approve")]
-    [ApiVersion("1.0")]
-    // [Authorize(Roles = $"{Roles.Admin},{Roles.Approver}")]
-    [AuthApproverAttribute]
-    [Produces("application/json")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [SwaggerResponse(200, "The execution was successful")]
-    [SwaggerResponse(400, "The request was invalid")]
-    [SwaggerResponse(401, "Unauthorized access attempted")]
-    public ActionResult<bool> ApproveOne(CriterionDto dto)
-    {
-        if (!ModelState.IsValid) return ValidationProblem(ModelState);
-        int success = -1;
-        try
-        {
-            success = _criterionService.Approve(dto);
-        }
-        catch (CustomException ex)
-        {
-            // TODO: handle more gracefully
-            return BadRequest(ex);
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex);
-        }
-        return Ok(Convert.ToBoolean(success));
-    }
 }
--- API/API.ProcureAccess/Controllers/ProductsController.cs
+++ API/API.ProcureAccess/Controllers/ProductsController.cs
@@ -12,44 +12,4 @@ public class ProductsController : BaseCrudController<Product, ProductDto, Produc
     {
         _productService = service;
     }
-
-    /// <summary>
-    /// Updates & approves a single record
-    /// </summary>
-    /// <remarks>
-    /// <pre>
-    /// </pre>
-    /// </remarks>
-    /// <param name="dto">Dto Entity to update</param>
-    /// <returns>Single record</returns>
-    [HttpPut("approve")]
-    [ApiVersion("1.0")]
-    // [Authorize(Roles = $"{Roles.Admin},{Roles.Approver}")]
-    [AuthApproverAttribute]
-    [Produces("application/json")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-    [SwaggerResponse(200, "The execution was successful")]
-    [SwaggerResponse(400, "The request was invalid")]
-    [SwaggerResponse(401, "Unauthorized access attempted")]
-    public ActionResult<bool> ApproveOne(ProductDto dto)
-    {
-        if (!ModelState.IsValid) return ValidationProblem(ModelState);
-        int success = -1;
-        try
-        {
-            success = _productService.Approve(dto);
-        }
-        catch (CustomException ex)
-        {
-            // TODO: handle more gracefully
-            return BadRequest(ex);
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex);
-        }
-        return Ok(Convert.ToBoolean(success));
-    }
 }
