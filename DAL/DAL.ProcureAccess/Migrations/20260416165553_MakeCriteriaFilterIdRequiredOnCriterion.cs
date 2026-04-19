cb6bf4bbbaee455f196969f63a683fbf01c97ef8
philipphoeninger <44400490+philipphoeninger@users.noreply.github.com>
2026-04-19 15:56:48 +0200

    add CriteriaFilterId required migration

---
+++ DAL/DAL.ProcureAccess/Migrations/20260416165553_MakeCriteriaFilterIdRequiredOnCriterion.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DAL.ProcureAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeCriteriaFilterIdRequiredOnCriterion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Criteria_CriteriaFilters_CriteriaFilterId",
+                schema: "dbo",
+                table: "Criteria");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CriteriaFilterId",
+                schema: "dbo",
+                table: "Criteria",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true)
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CriteriaAudit")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom")
+                .OldAnnotation("SqlServer:IsTemporal", true)
+                .OldAnnotation("SqlServer:TemporalHistoryTableName", "CriteriaAudit")
+                .OldAnnotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .OldAnnotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                .OldAnnotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Criteria_CriteriaFilters_CriteriaFilterId",
+                schema: "dbo",
+                table: "Criteria",
+                column: "CriteriaFilterId",
+                principalSchema: "dbo",
+                principalTable: "CriteriaFilters",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Criteria_CriteriaFilters_CriteriaFilterId",
+                schema: "dbo",
+                table: "Criteria");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CriteriaFilterId",
+                schema: "dbo",
+                table: "Criteria",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "CriteriaAudit")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom")
+                .OldAnnotation("SqlServer:IsTemporal", true)
+                .OldAnnotation("SqlServer:TemporalHistoryTableName", "CriteriaAudit")
+                .OldAnnotation("SqlServer:TemporalHistoryTableSchema", "dbo")
+                .OldAnnotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                .OldAnnotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Criteria_CriteriaFilters_CriteriaFilterId",
+                schema: "dbo",
+                table: "Criteria",
+                column: "CriteriaFilterId",
+                principalSchema: "dbo",
+                principalTable: "CriteriaFilters",
+                principalColumn: "Id");
+        }
+    }
+}
