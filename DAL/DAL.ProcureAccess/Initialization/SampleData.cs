5023bf6ac2702759b156146a43be947fac2b3a6e
philipphoeninger <44400490+philipphoeninger@users.noreply.github.com>
2026-05-04 17:51:36 +0200

    translate dummy product descriptions

--- DAL/DAL.ProcureAccess/Initialization/SampleData.cs
+++ DAL/DAL.ProcureAccess/Initialization/SampleData.cs
@@ -5,7 +5,7 @@ public static class SampleData
     public static List<User> Users => new()
     {
         new() 
-        { 
+        {
             UserName = "hoeninger@werte.it",
             Email = "hoeninger@werte.it",
             UICustomization = new(),
@@ -204,23 +204,23 @@ public static class SampleData
 
     public static List<Product> Products => new()
     {
-        new() { Id = 1, Name = "CoreFlow", Link = "www.coreflow.org", Description = "A description for...", IsDeleted = false },
-        new() { Id = 2, Name = "IntraSphere", Link = "www.intrasphere.com", Description = "A description for...", IsDeleted = false },
-        new() { Id = 3, Name = "ProximaSuite", Link = "www.proximasuite.com", Description = "A description for...", IsDeleted = false },
-        new() { Id = 4, Name = "WorkBridge", Link = "www.workbridge.com", Description = "A description for...", IsDeleted = false },
-        new() { Id = 5, Name = "SyntraLogic", Link = "www.syntralogic.it", Description = "A description for...", IsDeleted = false },
-        new() { Id = 6, Name = "Nexaro", Link = "www.nexaro.de", Description = "A description for...", IsDeleted = false },
-        new() { Id = 7, Name = "FlowMatrix", Link = "www.flowmatrix.com", Description = "A description for...", IsDeleted = false },
-        new() { Id = 8, Name = "OptiCore", Link = "www.opticore.com", Description = "A description for...", IsDeleted = false },
-        new() { Id = 9, Name = "BizStream", Link = "www.bizstream.ru", Description = "A description for...", IsDeleted = false },
-        new() { Id = 10, Name = "NovaDesk", Link = "www.novadesk.at", Description = "A description for...", IsDeleted = false },
-        new() { Id = 11, Name = "ClarioOne", Link = "www.clarioone.fr", Description = "A description for...", IsDeleted = false },
-        new() { Id = 12, Name = "FlexiSuite", Link = "www.flexisuite.com", Description = "A description for...", IsDeleted = false },
-        new() { Id = 13, Name = "EvoManage", Link = "www.evomanage.com", Description = "A description for...", IsDeleted = false },
-        new() { Id = 14, Name = "TaskFusion", Link = "www.taskfusion.com", Description = "A description for...", IsDeleted = false },
-        new() { Id = 15, Name = "PrimeHub", Link = "www.primehub.org", Description = "A description for...", IsDeleted = false },
-        new() { Id = 16, Name = "WorklineOS", Link = "www.worklineos.de", Description = "A description for...", IsDeleted = false },
-        new() { Id = 17, Name = "AquilaWare", Link = "www.aquilaware.com", Description = "A description for...", IsDeleted = false },
+        new() { Id = 1, Name = "CoreFlow", Link = "www.coreflow.org", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 2, Name = "IntraSphere", Link = "www.intrasphere.com", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 3, Name = "ProximaSuite", Link = "www.proximasuite.com", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 4, Name = "WorkBridge", Link = "www.workbridge.com", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 5, Name = "SyntraLogic", Link = "www.syntralogic.it", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 6, Name = "Nexaro", Link = "www.nexaro.de", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 7, Name = "FlowMatrix", Link = "www.flowmatrix.com", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 8, Name = "OptiCore", Link = "www.opticore.com", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 9, Name = "BizStream", Link = "www.bizstream.ru", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 10, Name = "NovaDesk", Link = "www.novadesk.at", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 11, Name = "ClarioOne", Link = "www.clarioone.fr", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 12, Name = "FlexiSuite", Link = "www.flexisuite.com", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 13, Name = "EvoManage", Link = "www.evomanage.com", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 14, Name = "TaskFusion", Link = "www.taskfusion.com", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 15, Name = "PrimeHub", Link = "www.primehub.org", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 16, Name = "WorklineOS", Link = "www.worklineos.de", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
+        new() { Id = 17, Name = "AquilaWare", Link = "www.aquilaware.com", Description = "Keine Beschreibung vorhanden", IsDeleted = false },
     };
 
     public static List<ProductCriteriaFilter> ProductCriteriaFilters => new()
